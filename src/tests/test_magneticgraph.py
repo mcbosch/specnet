8fd332e7cafc32b2a31fae39dce375e30f7f5d73
mcbosch <mciudadbosch@gmail.com>
2026-08-24 12:22:15 +0200

    Define tests for MagGraph - OK

--- src/tests/test_magneticgraph.py
+++ src/tests/test_magneticgraph.py
@@ -37,12 +37,40 @@ class TestMagGraph:
             raise ValueError(f"Edges must be 2-tuple, 3-tuple or 4-tuple")
         
         return u, v, k, d
+
+    @staticmethod
+    def assert_edge_in_adj(G, u, v, k, d, inverse=False):
+        r"""
+        Verifies if an edge is in _adj as expected. If inverse=True
+        checks for that edge and the inverse one.
+        """
+
+        assert v in G._adj[u], "Connection error"
+        assert k in G._adj[u][v], "Key error"
+        if inverse:
+            assert u in G._adj[v], "Connection error"
+            assert k in G._adj[v][u], "Key error"
+
+        dict_keys = d.keys()
+        dd = G[u][v][k]
+        assert len(d) == len(dd), "Not the same dict as expected"
+        for dk in dict_keys:
+            msg = "Attributes are not the same as expected"
+            assert dk in dd and dd[dk] == d[dk], msg
+            if inverse:
+                if dk == 'potential':
+                    assert dk in G._adj[v][u][k] and G._adj[v][u][k][dk] == -d[dk], msg
+                else:
+                    assert dk in G._adj[v][u][k] and G._adj[v][u][k][dk] == d[dk], msg
+
+        return True
+
     
     def compare_edges(self, e1, e2, inverted=False):
         n1, n2 = len(e1), len(e2)
         assert n1==n2, f"The edges {e1} and {e2} are not of the same length"
 
-        u1, v1, k1, d1 = self.get_edge_attr(e1)
+        u1, v1, k1, d1 = TestMagGraph.get_edge_attr(e1)
         u2, v2, k2, d2 = self.get_edge_attr(e2)
 
         if inverted:
@@ -88,3 +116,30 @@ class TestMagGraph:
         assert self.compare_edges(edges_H[0], e, inverted=False)
 
         assert self.compare_edges(edges_G[0], edges_G[1], inverted=True)
+
+    def test_add_edges_from(self):
+        e1 = (1,2,{'weight':2, 'potential':1})
+        e2 = (1,2,2)
+        e3 = (2,3,1,{'potential':-1})
+        ebunch = [e1, e2, e3]
+
+        G = MagGraph(sym=True)
+        H = MagGraph(sym=False)
+
+        G.add_edges_from(ebunch)
+        H.add_edges_from(ebunch)
+
+        assert len(G.edges()) == 2 * len(ebunch)
+        assert len(H.edges()) == len(ebunch)
+
+        assert self.assert_edge_in_adj(G, e1[0], e1[1], 0, e1[-1], inverse=True)
+        assert self.assert_edge_in_adj(G, e2[0], e2[1], 2, {}, inverse=True)
+        assert self.assert_edge_in_adj(G, e3[0], e3[1], 1, e3[-1], inverse=True)
+
+        assert self.assert_edge_in_adj(H, e1[0], e1[1], 0, e1[-1], inverse=False)
+        assert self.assert_edge_in_adj(H, e2[0], e2[1], 2, {}, inverse=False)
+        assert self.assert_edge_in_adj(H, e3[0], e3[1], 1, e3[-1], inverse=False)
+
+        
+
+        
