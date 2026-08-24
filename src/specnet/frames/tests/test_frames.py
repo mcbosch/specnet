0a9734e5277dd9d8f12b11cd57d39902934f1e85
mcbosch <mciudadbosch@gmail.com>
2026-08-24 13:54:33 +0200

    Add TODO tests

---
+++ src/specnet/frames/tests/test_frames.py
@@ -0,0 +1,18 @@
+import pytest
+import specnet as spn
+
+# TODO
+class TestFrames:
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_frame_construction(self):
+        pass
+
+    def test_2frames_join(self):
+        pass
+
+    def test_3frames_join(self):
+        pass
---
+++ src/specnet/frames/tests/test_frames_spectrum.py
@@ -0,0 +1,12 @@
+import pytest
+import specnet as spn
+
+# TODO
+class TestFramesSpec:
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_2frames_isospectrality(self):
+        pass
