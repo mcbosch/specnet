904ea82b97a8452523111e6306ee4af0c2bca82a
mcbosch <mciudadbosch@gmail.com>
2026-08-25 09:11:23 +0200

    Add save and read functions

--- src/specnet/utils.py
+++ src/specnet/utils.py
@@ -1,21 +1,29 @@
 from specnet.magraph import MagGraph
+from networkx import Graph
 
 import json
 import os
 import sys
 
-def _cache_clear():
-    pass
+def read_graph(path, typeg) -> MagGraph:
+    with open(f"{path}", "r") as f:
+        adj = json.load(f)
 
-def read_graph(path) -> MagGraph:
-    pass
+    G = typeg()
+    G._adj = adj
+    return G    
 
-def save_graph(G : MagGraph=None, path : str=None) -> str:
-    r"""
-    
-    """
-    if G is None: raise ValueError
-    if path is None: raise ValueError
+def save_graph(G : MagGraph=None, path : str=None, name : str = None) -> str:
 
-    ebunch = G.edges(data=True, keys=True)
-    
+    if not isinstance(G, Graph) : raise ValueError(f"{type(G)} should be a graph object")
+    if path is None: raise ValueError("Introduce a path where save the graph")
+
+    if G.is_multigraph():
+        ebunch = G.edges(data=True, keys=True)
+    else:
+        ebunch = G.edges(data=True)
+
+    with open(f"{path}/{name}.json", "w") as f:
+        json.dump(G._adj, f, indent=4)
+
+    return f"{path}/{name}"
